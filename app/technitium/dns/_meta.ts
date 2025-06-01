fd2a487ff2a4a0e9f3813ed9c695dbd05d8213c5
teerakanotk <teerakanotk@outlook.com>
2025-06-01 16:21:19 +0700

    Initial commit

---
+++ app/technitium/dns/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: "Setup",
+  },
+  "add-record": {
+    title: "Add Record",
+  },
+  record: {
+    title: "Record Type",
+  },
+};
---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  search: false,
+});
+export default withNextra();
