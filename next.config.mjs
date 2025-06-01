861906bca3f120d6add5b822df009248fb522f60
teerakanotk <teerakanotk@outlook.com>
2025-06-01 17:07:39 +0700

    Add disabling linting during production builds

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,11 @@ import nextra from "nextra";
 const withNextra = nextra({
   search: false,
 });
-export default withNextra();
+
+export default withNextra({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+});
