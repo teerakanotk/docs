5d43a750417681592ca52547b222a831e70c9d71
teerakanotk <teerakan.otk@outlook.com>
2025-06-06 20:13:11 +0700

    fix: default website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
   title: {
+    default: "CTS Documents",
     template: "%s | CTS Documents",
   },
 };
